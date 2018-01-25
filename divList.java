8e5e1d1eb3759d8068f92cc124c0f289ea6fa9ab
Youssef <agy292@email.vccs.edu>
2018-01-25 13:53:32 -0500

    divList

---
+++ divList.java
@@ -0,0 +1,25 @@
+public class divList
+{
+	public static void main(String[] args)
+	{
+		int count = 0;
+		for (int i = 100; i<=1000; i++)
+		{
+			
+			if(i % 5==0 && i%6 ==0)
+			{
+				
+				count++;
+				if(count >=10)
+				{
+					System.out.println(" " + i);
+					count = 0;
+				}
+				else
+					System.out.print(" " + i);	
+					
+				
+			}
+		}
+	}
+}
